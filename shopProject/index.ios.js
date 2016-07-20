c55f0114113d68bb77be3b0e53efda215b2f674e
gonghonggao <gong_honggao@163.com>
2016-07-20 17:11:36 +0800

    整合应用

--- shopProject/index.ios.js
+++ shopProject/index.ios.js
@@ -4,50 +4,17 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+'use strict';
+var React = require('react');
+var ReactNative = require('react-native');
+var App = require('./app/index.js');
 
-class shopProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var shopProject = React.createClass({
+    render() {
+        return(
+            <App />
+        );
+    }
 });
 
-AppRegistry.registerComponent('shopProject', () => shopProject);
+ReactNative.AppRegistry.registerComponent('shopProject', () => shopProject);
